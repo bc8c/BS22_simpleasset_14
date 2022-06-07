3ba73e896e08fa55679bea4aecfb2f58502e5678
bc8c <bc8c@naver.com>
2022-06-07 13:26:16 +0000

    change simpleasset server.js

--- simpleasset/application/server.js
+++ simpleasset/application/server.js
@@ -40,7 +40,7 @@ app.get('/transfer', (request,response)=>{
 
 // REST API 라우팅
 
-app.post('/user', (request,response)=>{
+app.post('/user', async(request,response)=>{
 
     const mode = request.body.mode
     console.log('/user-post- '+mode)
@@ -71,29 +71,150 @@ app.post('/user', (request,response)=>{
             if (adminExists) {
                 console.log('An identity for the admin user "admin" already exists in the wallet');
                 // client( web browser )에게 오류를 전송
-                const obj = JSON.parse('{"ERR_MSG":"An identity for the admin user "admin" already exists in the wallet"}')
+                const obj = JSON.parse('{"ERR_MSG":"An identity for the admin user admin already exists in the wallet"}')                
+                
                 response.status(400).json(obj);
+                return
             }
 
             // Enroll the admin user, and import the new identity into the wallet.
             const enrollment = await ca.enroll({ enrollmentID: id, enrollmentSecret: pw });
             const identity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
             wallet.import('admin', identity);
-            console.log('Successfully enrolled admin user "admin" and imported it into the wallet');
-            const obj = JSON.parse('{"PAYLOAD":"Successfully enrolled admin user "admin" and imported it into the wallet"}')
+            console.log('Successfully enrolled admin user admin and imported it into the wallet');
+            const obj = JSON.parse('{"PAYLOAD":"Successfully enrolled admin user admin and imported it into the wallet"}');
             response.status(200).json(obj);
             
         } catch (error) {
             console.error(`Failed to enroll admin user "admin": ${error}`);
             // client( web browser )에게 오류를 전송
-            const obj = JSON.parse('{"ERR_MSG":"Failed to enroll admin user "admin": ${error}"}')
+            const obj = JSON.parse('{"ERR_MSG":"Failed to enroll admin user admin: ${error}"}')
             response.status(400).json(obj);
         }
     }
     else if(mode == 2){ // 사용자 인증서
 
+        const id = request.body.id;
+        const role = request.body.role;
+        console.log('/user-post- '+id+'-',+role)
+
+        try {
+
+            // Create a new file system based wallet for managing identities.
+            const walletPath = path.join(process.cwd(), 'wallet');
+            const wallet = new FileSystemWallet(walletPath);
+            console.log(`Wallet path: ${walletPath}`);
+    
+            // Check to see if we've already enrolled the user.
+            const userExists = await wallet.exists(id);
+            if (userExists) {
+                console.log('An identity for the user "user1" already exists in the wallet');
+                // client( web browser )에게 오류를 전송
+                const obj = JSON.parse('{"ERR_MSG":"An identity for the user ${id} already exists in the wallet"}')                                       
+                response.status(400).json(obj);
+                return
+            }
+    
+            // Check to see if we've already enrolled the admin user.
+            const adminExists = await wallet.exists('admin');
+            if (!adminExists) {
+                console.log('An identity for the admin user "admin" does not exist in the wallet');
+                console.log('Run the enrollAdmin.js application before retrying');
+                // client( web browser )에게 오류를 전송
+                const obj = JSON.parse('{"ERR_MSG":"An identity for the admin user admin does not exists in the wallet"}')
+                response.status(400).json(obj);
+                return
+            }
+    
+            // Create a new gateway for connecting to our peer node.
+            const gateway = new Gateway();
+            await gateway.connect(ccp, { wallet, identity: 'admin', discovery: { enabled: false } });
+    
+            // Get the CA client object from the gateway for interacting with the CA.
+            const ca = gateway.getClient().getCertificateAuthority();
+            const adminIdentity = gateway.getCurrentIdentity();
+    
+            // Register the user, enroll the user, and import the new identity into the wallet.
+            const secret = await ca.register({ affiliation: 'org1.department1', enrollmentID: id, role: role }, adminIdentity);
+            const enrollment = await ca.enroll({ enrollmentID: id, enrollmentSecret: secret });
+            const userIdentity = X509WalletMixin.createIdentity('Org1MSP', enrollment.certificate, enrollment.key.toBytes());
+            wallet.import(id, userIdentity);
+            console.log('Successfully registered and enrolled user and imported it into the wallet');
+            const obj = JSON.parse('{"PAYLOAD":"Successfully enrolled admin user admin and imported it into the wallet"}')
+            response.status(200).json(obj);
+    
+        } catch (error) {
+            console.error(`Failed to register user ${id}: ${error}`);
+            // client( web browser )에게 오류를 전송
+            const obj = JSON.parse('{"ERR_MSG":"Failed to register user ${id}: ${error}"}')
+            response.status(400).json(obj);
+        }
+
     }
 })
+
+// 자산생성 post : '/asset'
+app.post('/asset', async(request,response)=>{
+
+    const id = request.body.id;
+    const key = request.body.key;
+    const value = request.body.value;
+    console.log('/asset-post- '+id+'-',+key+'-',+value)
+
+    // wallet에 있는 사용자 인증서 가져오기
+
+    // Create a new file system based wallet for managing identities.
+    const walletPath = path.join(process.cwd(), 'wallet');
+    const wallet = new FileSystemWallet(walletPath);
+    console.log(`Wallet path: ${walletPath}`);
+
+    // Check to see if we've already enrolled the user.
+    const userExists = await wallet.exists(id);
+    if (!userExists) {
+        console.log('An identity for the user ${id} does not exists in the wallet');
+        // client( web browser )에게 오류를 전송
+        const obj = JSON.parse('{"ERR_MSG":"An identity for the user ${id} does not exists in the wallet"}')                                       
+        response.status(400).json(obj);
+        return
+    }
+    
+    // 블록체인 네트워크에 접속 ( peer1 에 접속 )
+
+    // 1. 게이트웨이 접속
+    const gateway = new Gateway();
+    await gateway.connect(ccp, { wallet, identity: id, discovery: { enabled: false } });
+
+    // 2. 채널( mychannel ) 접속
+    const network = await gateway.getNetwork('mychannel');
+
+    // 3. 체인코드 가져오기 ( simpleasset )
+    const contract = network.getContract('simpleasset');
+
+
+    // 4. 체인코드 호출하기 ( set ( key, value ) )
+    await contract.submitTransaction('set', key, value);
+    console.log('Transaction has been submitted');
+
+    // 5. 게이트웨이 연결 해제
+    await gateway.disconnect();
+
+
+    // 수행 결과를 client에게 전달!
+
+    const resultPath = path.join(process.cwd(), '/views/result.html')
+    var resultHTML = fs.readFileSync(resultPath, 'utf8')
+    resultHTML = resultHTML.replace("<div></div>", "<div><p>Transaction has been submitted</p></div>")
+    response.status(200).send(resultHTML)
+
+})
+
+// 자산조회 get : '/asset'
+
+// 히스토리조회 get : '/assets'
+
+// 송금 post : '/tx'
+
+
 
 
 
